38d923360f9a989fffce8bea84d7b66d3e943445
chenaho <chenaho@gmail.com>
2012-07-19 17:55:43 +0800

    log

--- stereogauge.bat
+++ stereogauge.bat
@@ -1,5 +1,4 @@
-@echo StereoGauge.exe C:\xampp\htdocs\st\stereogaugeV2\img_0037.png C:\xampp\htdocs\st\stereogaugeV2\img_0038.png 167 240 167 240 -1.713752 1.448144 -0.396208 -1.923150 1.461892 -0.400685 0
-REM StereoGauge img_0037.png img_0038.png 167 240 167 240 -1.713752 1.448144 -0.396208 -1.923150 1.461892 -0.400685 0
-REM StereoGauge <image1> <image2> <pos1_X> <pos1_Y> <pos2_X> <pos2_Y> <Y1> <Y1> <Y1> <Y2> <Y2> <Y2> <IsiPhone4S> <lat_1> <long_1> <lat_2> <long_2> <degree> <gps strength>
-REM StereoGauge img_0037.png img_0038.png 167 240 167 240 -1.713752 1.448144 -0.396208 -1.923150 1.461892 -0.400685 0 1.12458 102.354 1.13458 102.154 40.5 2.1
+
+REM StereoGauge <image1> <image2> <pos1_X> <pos1_Y> <pos2_X> <pos2_Y> <Y1> <Y1> <Y1> <Y2> <Y2> <Y2> <IsiPhone4S> <lat_1> <long_1> <lat_2> <long_2> <degree> <gps strength
+StereoGauge.exe ../uploads/DE_1342668088_rfimage_0.jpg ../uploads/DE_1342668088_rfimage_1.jpg 167.000000 240.000000 167.000000 240.000000 -0.235087 1.510378 -0.158762 -0.656548 1.490556 -0.148220 0
 pause
